49c5b8304ebcfbe0c25800a12b835cd0eb49f524
KashifQureshi <CreativeKashifQ@gmail.com>
2021-10-19 17:11:31 -0700

    book store api updated

---
+++ app/Http/Controllers/BookStoreController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookStore;
+use Illuminate\Http\Request;
+
+class BookStoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $books = BookStore::all();
+        return response()->json($books);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'cost' => 'required',
+        ]);
+
+        $book = new BookStore();
+        $book->title = $request->title;
+        $book->author = $request->author;
+        $book->cost = $request->cost;
+        $book->save();
+        return response()->json(['success' => 'Book Save Successfully In BookStore']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BookStore $book)
+    {
+
+        return response()->json($book);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BookStore $book)
+    {
+        //
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BookStore $book)
+    {
+        //
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'cost' => 'required',
+        ]);
+
+        $$book->title = $request->title;
+        $$book->author = $request->author;
+        $$book->cost = $request->cost;
+        $$book->update();
+        return response()->json(['success' => 'Book Updated Successfully In BookStore']);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BookStore  $bookStore
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BookStore $book)
+    {
+        $book->delete();
+        return response()->json(['success' => 'Book Deleted Successfully From Book Store']);
+    }
+}
